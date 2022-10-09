9fa7d9ca9120284fb668c75e742dfba9bff14fd4
strangerthingsseries123 <115345148+strangerthingsseries123@users.noreply.github.com>
2022-10-09 12:19:41 +0530

    Add files via upload

---
+++ aaignmnet 2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a;    // decalre the variable
+	a = 10;  //initialize a variable
+	printf("value of the variable is = %d ",a);
+	int b,c,d,f ; //declare the variable 
+	b = 15 ;
+	c = 16 ;
+	d = 17 ;
+	f = 18 ; // initialize the variable
+	printf("and first variable holds an integer contant = %d",b);
+	printf("and second variable holds an integer contant = %d",c);
+	printf("and third variable holds an integer contant = %d",d);
+	printf("and fourth variable holds an integer constant = %d",f);
+	return 0;
+	
+}
---
+++ assingment 1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("hello");
+	printf("HELLO");
+	printf("HELLO WROLD");
+	printf("maheep paliwal");
+	printf("tecno njr inia");
+	printf("hello\nwrold");
+	printf("maheep\npaliwal");
+	return 0;
+	
+}
+
