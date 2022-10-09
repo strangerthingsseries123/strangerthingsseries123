0fbe7ca727ef1c461f1b9dd3841e42d37add223e
strangerthingsseries123 <115345148+strangerthingsseries123@users.noreply.github.com>
2022-10-09 21:28:04 +0530

    Add files via upload

---
+++ assignment 3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+ 	int a,b,c; 
+ 	printf("enter the value of the first  variable = ");
+ 	scanf("%d",&a);
+ 	printf("enter the value of the second   variable = ");
+ 	scanf("%d",&b);
+ 	printf("enter the value of the third  variable = ");
+ 	scanf("%d",&c);
+ 	printf("value of the first variable is = %d",a);
+ 	printf("value of the second variable is = %d",b);
+ 	printf("value of the third variable is = %d",c);s
+ 	return 0;
+ }
